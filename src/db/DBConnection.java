8b873701583ecdf64a1aa680b6575bcea89bb2a7
Mihir <104824672+MihirKalani@users.noreply.github.com>
2026-05-05 10:07:22 +0530

    update
    Removed sensitive password from DBConnection class.

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String url = "jdbc:mysql://127.0.0.1:3306/bus_reservation";
     private static final String username = "root";
-    private static final String password = "mihir@2004";
+    private static final String password = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, username, password);
